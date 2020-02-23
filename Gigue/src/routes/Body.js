6b95905dc6f27779fb19ec336b321eac11acea2f
Molly Novash <novash@pdx.edu>
2020-02-23 15:08:44 -0800

    added a help page linked in the header

--- Gigue/src/routes/Body.js
+++ Gigue/src/routes/Body.js
@@ -1,13 +1,15 @@
-import { NavLink, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import React from "react";
 import "../styles/App.css";
 import Landing from "./Landing";
 import Events from "./Events";
+import Help from "./Help";
 
 const Body = () => (
   <Switch>
     <Route exact path="/" component={Landing}></Route>
     <Route exact path="/Events" component={Events}></Route>
+    <Route exact path="/Help" component={Help}></Route>
   </Switch>
 );
 
