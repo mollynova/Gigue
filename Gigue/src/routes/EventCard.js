4dd739dc00a4cd637b7f081d7708b60059e5074d
FinchHop <wolakal@pdx.edu>
2020-02-29 21:38:27 -0800

    Fixed cards, added flip upon hover

--- Gigue/src/routes/EventCard.js
+++ Gigue/src/routes/EventCard.js
@@ -5,42 +5,43 @@ import React from 'react';
 
 const EventInfo = props => {
 
-      const rows = props.eventsData.map((row, index) => {
-        console.log(row.eventName);
-        return (
-          <div className="card-containers" key={index}>
-            <div className="card card-flip border-primary">
-              <div className="front">
-                <div className="card-body">
-                <h5 className="card-title">{row.EventName}</h5>
-                        <p className="card-text" id="performance-dates">{row.StartDate}</p>
-                        <a href="#" className="btn btn-primary">Flip for More Info</a> 
-                </div>
-              </div>
-            </div>
-            <div className="back">
-              <div className="card-body">
+  const rows = props.eventsData.map((row, index) => {
+    console.log(row.eventName);
+    return (
+      <div className="card-containers" key={index}>
+        <div className="card card-flip border-primary">
+          <div className="front">
+            <div className="card-body">
               <h5 className="card-title">{row.EventName}</h5>
-                        <h6 className="card-subtitle mb-2 text-muted">{row.StartDate}</h6>
-                        <p className="card-text" id="performance-dates">{row.Venue}</p>
-                        <a href={row.Uri} className="card-link">Click to Buy Tickets</a>
-              </div>
+              <p className="card-text" id="performance-dates">Date: {row.StartDate}</p>
+              <button href="#" className="btn-primary">Flip for More Info!</button>
             </div>
           </div>
-        );
-      });
-      return <div>{rows}</div>;
 
-  };
+          <div className="back">
+            <div className="card-body">
+              <h5 className="card-title">{row.EventName}</h5>
+              <h6 className="card-subtitle mb-2 text-muted">{row.StartDate}</h6>
+              <p className="card-text" id="performance-dates">{row.Venue}</p>
+              <a href={row.Uri} className="card-link">Click to Buy Tickets</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  });
+  return <div>{rows}</div>;
+
+};
 
 
 class EventCard extends React.Component {
-    render() {
-      console.log(this.props);
+  render() {
+    console.log(this.props);
 
-      const { eventsData } = this.props;
-      return <EventInfo eventsData={eventsData} />;
-    }
+    const { eventsData } = this.props;
+    return <EventInfo eventsData={eventsData} />;
   }
-  
-  export default EventCard;
+}
+
+export default EventCard;
