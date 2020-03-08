04dec8cbafdc2fa15db635e3562a14320530724e
FinchHop <wolakal@pdx.edu>
2020-03-07 21:35:56 -0800

    added styling to artist page

--- Gigue/src/routes/ArtistPage.js
+++ Gigue/src/routes/ArtistPage.js
@@ -1,5 +1,17 @@
 import React from "react";
 import "../styles/App.css";
+import "../styles/artist.css";
+
+const ArtistInfo = props => {
+    //Call Api here? Connect to Spotify
+    console.log(props);
+    return(
+        <div className="artist-page">
+            <h1>{props.artistPage}</h1>
+        </div>
+        
+    );
+}
 
 class ArtistPage extends React.Component{
     state = {
@@ -10,7 +22,8 @@ class ArtistPage extends React.Component{
         const artist = this.props.location.state.artist;
         console.log(this.props);
         return(
-            <h2>{artist}</h2>
+
+            <ArtistInfo artistPage={artist}></ArtistInfo>
             
         )
     }
